4980da21e3d7d44c24282c380fdfb07cc47a63b4
avi <avinash3.verma@gmail.com>
2021-07-20 09:03:31 +0530

    introduce stack

---
+++ src/learn/com/Practice_Stack.java
@@ -0,0 +1,17 @@
+package learn.com;
+
+import java.util.Stack;
+
+public class Practice_Stack {
+
+	public static void main(String[] args) {
+			Stack <Integer> stack = new Stack<>();
+			stack.push(7);
+			stack.push(5);
+			stack.push(2);
+			System.out.println(stack.peek());
+			System.out.println(stack.pop());
+			System.out.println(stack);
+	}
+
+}
